845d02ead0eed3ce311ee88923507875676d4923
isirmt <52734487+isirmt@users.noreply.github.com>
2026-05-25 22:52:02 +0900

    update: ref ui (search window)

--- frontend/src/components/works/searchWindow.tsx
+++ frontend/src/components/works/searchWindow.tsx
@@ -1,11 +1,36 @@
 "use client";
 
+import { useEffect, useRef, useState } from "react";
+
 export default function SearchWindow() {
+  const [isOpen, setIsOpen] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (containerRef.current && !containerRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      } else {
+        setIsOpen(true);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="pointer-events-none fixed top-0 left-0 z-100 flex size-full flex-col items-end bg-transparent p-6">
-      <div className="relative h-12 w-78">
+    <div ref={containerRef} className="pointer-events-none fixed top-0 gap-6 left-0 z-100 flex size-full flex-col items-end bg-transparent p-6">
+      <div className="relative h-12 w-78 z-10">
         <input className="font-noto pointer-events-auto block h-12 w-78 rounded-full border border-[#ccc] bg-white/60 px-4 py-1 text-[#333] shadow-md shadow-[#ccc] backdrop-blur-2xl hover:border-[#777]" />
       </div>
+      {isOpen && (
+        <div className={`z-0 pointer-events-auto relative p-4 h-64 w-120 bg-white/85 border rounded border-[#ccc] backdrop-blur-2xl`}>
+
+        </div>
+      )}
     </div>
   );
 }
