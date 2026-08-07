7db6e85232434f633de6e3655151143d0bd5e05d
isirmt <52734487+isirmt@users.noreply.github.com>
2026-08-07 14:22:51 +0900

    delete: hero virtical images

--- frontend/src/components/heroSection.tsx
+++ frontend/src/components/heroSection.tsx
@@ -25,7 +25,7 @@ export default function HeroSection() {
       <div
         className={`absolute top-0 left-0 size-full transition-all duration-300 ${selectingWork ? "opacity-0" : "opacity-100"}`}
       >
-        <VerticalViewer />
+        {/* <VerticalViewer /> */}
       </div>
       <div
         className={`absolute top-0 left-0 size-full overflow-hidden bg-white transition-all duration-300 ${selectingWork ? "opacity-100 delay-300" : "opacity-0"}`}
