2a4b87d7404dade424878888f5e7d5d19768710b
isirmt <52734487+isirmt@users.noreply.github.com>
2026-08-11 01:41:07 +0900

    update: header style

--- frontend/src/components/public/header.tsx
+++ frontend/src/components/public/header.tsx
@@ -18,9 +18,9 @@ export default function Header() {
   );
 
   return (
-    <header className="pointer-events-none fixed top-0 left-0 z-999 flex h-32 w-full items-center justify-between bg-transparent px-6 md:px-8">
+    <header className="pointer-events-none fixed top-0 left-0 z-999 flex h-24 w-full items-center justify-between bg-transparent px-3 md:h-32 md:px-8">
       <Link href="/" className="pointer-events-auto" onClick={handleLogoClick}>
-        <img src="/isirmt_logo.png" alt="ISIRMT Logo" className="h-28 w-auto" />
+        <img src="/isirmt_logo.png" alt="ISIRMT Logo" className="h-20 w-auto" />
       </Link>
     </header>
   );
