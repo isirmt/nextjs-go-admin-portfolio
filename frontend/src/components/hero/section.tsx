9f03d56043121160453a5cb7d0672649e86552c8
isirmt <52734487+isirmt@users.noreply.github.com>
2026-08-11 19:04:53 +0900

    update: select none (button)

--- frontend/src/components/hero/section.tsx
+++ frontend/src/components/hero/section.tsx
@@ -41,7 +41,7 @@ function HeroPageNavigation() {
             <button
               type="button"
               onClick={() => goTo(page.id)}
-              className={`font-dot relative z-10 flex size-10 items-center justify-center rounded border-2 border-[#F43F5E] text-xl font-bold shadow transition-all ${isCurrent ? "pointer-events-none rotate-45 bg-[#F43F5E] text-white" : "cursor-pointer bg-white text-[#F43F5E]"}`}
+              className={`font-dot relative z-10 flex size-10 items-center justify-center rounded border-2 border-[#F43F5E] text-xl font-bold shadow transition-all select-none ${isCurrent ? "pointer-events-none rotate-45 bg-[#F43F5E] text-white" : "cursor-pointer bg-white text-[#F43F5E]"}`}
             >
               <span
                 className={`transition-all ${isCurrent ? "-rotate-45" : ""}`}
