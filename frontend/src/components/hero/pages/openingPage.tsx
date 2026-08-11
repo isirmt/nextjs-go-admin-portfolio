2fa90336b55a563cf12f6f6549105be74d72b4fb
isirmt <52734487+isirmt@users.noreply.github.com>
2026-08-12 07:03:06 +0900

    update: auto colorful

--- frontend/src/components/hero/pages/openingPage.tsx
+++ frontend/src/components/hero/pages/openingPage.tsx
@@ -1,13 +1,27 @@
 "use client";
 import { lineSeedJp } from "@/lib/fonts";
 import HeroPageFrame from "../pageFrame";
+import { useEffect, useState } from "react";
 
 export default function OpeningHeroPage() {
+  const [isColorful, setIsColorful] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsColorful(true);
+    }, 2300);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <HeroPageFrame>
       <div
         className={`pointer-events-none absolute inset-0 flex items-center justify-center bg-[#ffffff] font-bold select-none ${lineSeedJp.className}`}
       >
+        <div
+          className={`absolute z-10 aspect-square rounded-full bg-transparent backdrop-grayscale-100 transition-all duration-1000 ${isColorful ? "size-0" : "size-[calc(max(100dvh,100dvw)*1.414)]"}`}
+        />
         <div className="animate-btt absolute top-[300dvh] right-0 h-[300dvh] w-[300dvh] rounded-full bg-red-100 md:right-1/3" />
         <div className="animate-btt absolute top-[300dvh] left-0 h-[300dvh] w-[300dvh] rounded-full bg-blue-100 [animation-delay:0.5s] md:left-1/3" />
         <div className="flex flex-col items-center justify-center gap-6">
@@ -24,10 +38,15 @@ export default function OpeningHeroPage() {
             </div>
           </div>
           <div className="flex flex-row items-center justify-center gap-12">
-            <div className="relative flex size-20 items-center justify-center">
-              <div className="flex size-full items-center justify-center bg-[#F4CE6B] text-7xl leading-none text-white">
+            <div className="group relative flex size-20 items-center justify-center">
+              <button
+                className={`ease-over pointer-events-auto z-20 flex size-full cursor-pointer items-center justify-center text-7xl leading-none text-white transition-[scale,background-color] duration-[150ms,1000ms] hover:scale-90 active:scale-80 ${
+                  isColorful ? "bg-[#aaa]" : "bg-[#F4CE6B]"
+                }`}
+                onClick={() => setIsColorful((current) => !current)}
+              >
                 と
-              </div>
+              </button>
             </div>
           </div>
           <div className="flex flex-row items-center justify-center gap-12">
