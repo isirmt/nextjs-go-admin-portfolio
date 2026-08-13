2287d45be68cd50d4a11d5d257d48a819ac3f891
isirmt <52734487+isirmt@users.noreply.github.com>
2026-08-13 15:43:23 +0900

    update: hero wrapper

--- frontend/src/components/hero/pages/openingPage.tsx
+++ frontend/src/components/hero/pages/openingPage.tsx
@@ -24,28 +24,30 @@ export default function OpeningHeroPage() {
         <div
           className={`absolute z-10 aspect-square rounded-full bg-transparent backdrop-grayscale-100 transition-all duration-1500 ease-in-out ${isColorful ? "size-0" : "size-[calc(max(100dvh,100dvw)*1.414)]"}`}
         />
-        <div className="absolute -top-[85dvh] h-[180dvh] w-[180dvw] rounded-b-full bg-[linear-gradient(45deg,#d1fafa_40vw,#d1f0ff_140vw)]" />
-        <div className="animate-up-down-smooth absolute -top-[110dvh] h-[120dvh] w-[120dvw] translate-x-[30dvw] rounded-b-full bg-[linear-gradient(45deg,#baf8dd_40vw,#cdfefe_140vw)] [animation-delay:0.5s]" />
+        <div className="absolute top-0 left-0 flex size-full items-center justify-center">
+          <div className="absolute -top-[85dvh] h-[180dvh] w-[180dvw] rounded-b-full bg-[linear-gradient(45deg,#d1fafa_40vw,#d1f0ff_140vw)]" />
+          <div className="animate-up-down-smooth absolute -top-[110dvh] h-[120dvh] w-[120dvw] translate-x-[30dvw] rounded-b-full bg-[linear-gradient(45deg,#baf8dd_40vw,#cdfefe_140vw)] [animation-delay:0.5s]" />
 
-        <Sparkle
-          className={`animate-up-down-smooth absolute top-[15%] -left-[5%] aspect-3/4 w-40 bg-[#f8c774] sm:left-[5%] md:left-[10%] md:w-65`}
-        >
-          <div className="animate-up-down-smooth absolute bottom-0 left-0 size-[140%] -translate-x-1/2 translate-y-1/2 rounded-full bg-[#f6d992] duration-1000" />
-        </Sparkle>
-        <div
-          className={`animate-up-down-smooth absolute -top-[3%] -left-[3%] aspect-square w-30 overflow-hidden rounded-full bg-[#d29cef] [animation-delay:-.5s]`}
-        >
-          <div className="animate-up-down-smooth absolute right-0 bottom-0 size-[140%] translate-x-1/2 translate-y-1/2 rounded-full bg-[#d5c3f8] duration-1000 [animation-delay:-.5s]" />
-        </div>
-        <Sparkle
-          className={`animate-up-down-smooth absolute right-[20%] bottom-[-5%] aspect-1/2 w-38 bg-[#f494ae] [animation-delay:-.7s]`}
-        >
-          <div className="animate-up-down-smooth absolute right-0 bottom-0 size-[140%] translate-x-1/2 translate-y-1/2 rounded-full bg-[#f3c2e2] duration-1000 [animation-delay:-.7s]" />
-        </Sparkle>
-        <div
-          className={`animate-up-down-smooth absolute top-[25%] -right-[10%] aspect-square w-50 overflow-hidden rounded-full bg-[#9aebe6] [animation-delay:-.9s] sm:-right-[1%]`}
-        >
-          <div className="animate-up-down-smooth absolute bottom-0 left-0 size-[140%] -translate-x-1/2 translate-y-1/2 rounded-full bg-[#b0f3ee] duration-1000 [animation-delay:-.9s]" />
+          <Sparkle
+            className={`animate-up-down-smooth absolute top-[15%] -left-[5%] aspect-3/4 w-40 bg-[#f8c774] sm:left-[5%] md:left-[10%] md:w-65`}
+          >
+            <div className="animate-up-down-smooth absolute bottom-0 left-0 size-[140%] -translate-x-1/2 translate-y-1/2 rounded-full bg-[#f6d992] duration-1000" />
+          </Sparkle>
+          <div
+            className={`animate-up-down-smooth absolute -top-[3%] -left-[3%] aspect-square w-30 overflow-hidden rounded-full bg-[#d29cef] [animation-delay:-.5s]`}
+          >
+            <div className="animate-up-down-smooth absolute right-0 bottom-0 size-[140%] translate-x-1/2 translate-y-1/2 rounded-full bg-[#d5c3f8] duration-1000 [animation-delay:-.5s]" />
+          </div>
+          <Sparkle
+            className={`animate-up-down-smooth absolute right-[20%] bottom-[-5%] aspect-1/2 w-38 bg-[#f494ae] [animation-delay:-.7s]`}
+          >
+            <div className="animate-up-down-smooth absolute right-0 bottom-0 size-[140%] translate-x-1/2 translate-y-1/2 rounded-full bg-[#f3c2e2] duration-1000 [animation-delay:-.7s]" />
+          </Sparkle>
+          <div
+            className={`animate-up-down-smooth absolute top-[25%] -right-[10%] aspect-square w-50 overflow-hidden rounded-full bg-[#9aebe6] [animation-delay:-.9s] sm:-right-[1%]`}
+          >
+            <div className="animate-up-down-smooth absolute bottom-0 left-0 size-[140%] -translate-x-1/2 translate-y-1/2 rounded-full bg-[#b0f3ee] duration-1000 [animation-delay:-.9s]" />
+          </div>
         </div>
         <div className="animate-btt absolute top-[300dvh] right-0 h-[300dvh] w-[300dvh] rounded-full bg-[linear-gradient(180deg,#faddf1,#f5c2e5)] ease-out md:right-1/3" />
         <div className="animate-btt absolute top-[300dvh] left-0 h-[300dvh] w-[300dvh] rounded-full bg-[linear-gradient(180deg,#cbf9ff,#b2f2fb)] ease-out [animation-delay:0.5s] md:left-1/3" />
