18b885e75207af76cfb354e0e0ad51708481c77e
isirmt <52734487+isirmt@users.noreply.github.com>
2026-08-13 15:40:39 +0900

    update: objects

--- frontend/src/components/hero/pages/openingPage.tsx
+++ frontend/src/components/hero/pages/openingPage.tsx
@@ -28,7 +28,7 @@ export default function OpeningHeroPage() {
         <div className="animate-up-down-smooth absolute -top-[110dvh] h-[120dvh] w-[120dvw] translate-x-[30dvw] rounded-b-full bg-[linear-gradient(45deg,#baf8dd_40vw,#cdfefe_140vw)] [animation-delay:0.5s]" />
 
         <Sparkle
-          className={`animate-up-down-smooth absolute top-[15%] left-[20%] aspect-3/4 w-45 bg-[#f8c774]`}
+          className={`animate-up-down-smooth absolute top-[15%] -left-[5%] aspect-3/4 w-40 bg-[#f8c774] sm:left-[5%] md:left-[10%] md:w-65`}
         >
           <div className="animate-up-down-smooth absolute bottom-0 left-0 size-[140%] -translate-x-1/2 translate-y-1/2 rounded-full bg-[#f6d992] duration-1000" />
         </Sparkle>
@@ -42,8 +42,13 @@ export default function OpeningHeroPage() {
         >
           <div className="animate-up-down-smooth absolute right-0 bottom-0 size-[140%] translate-x-1/2 translate-y-1/2 rounded-full bg-[#f3c2e2] duration-1000 [animation-delay:-.7s]" />
         </Sparkle>
-        <div className="animate-btt absolute top-[300dvh] right-0 h-[300dvh] w-[300dvh] rounded-full bg-[#faddf1] ease-out md:right-1/3" />
-        <div className="animate-btt absolute top-[300dvh] left-0 h-[300dvh] w-[300dvh] rounded-full bg-[#cbf9ff] ease-out [animation-delay:0.5s] md:left-1/3" />
+        <div
+          className={`animate-up-down-smooth absolute top-[25%] -right-[10%] aspect-square w-50 overflow-hidden rounded-full bg-[#9aebe6] [animation-delay:-.9s] sm:-right-[1%]`}
+        >
+          <div className="animate-up-down-smooth absolute bottom-0 left-0 size-[140%] -translate-x-1/2 translate-y-1/2 rounded-full bg-[#b0f3ee] duration-1000 [animation-delay:-.9s]" />
+        </div>
+        <div className="animate-btt absolute top-[300dvh] right-0 h-[300dvh] w-[300dvh] rounded-full bg-[linear-gradient(180deg,#faddf1,#f5c2e5)] ease-out md:right-1/3" />
+        <div className="animate-btt absolute top-[300dvh] left-0 h-[300dvh] w-[300dvh] rounded-full bg-[linear-gradient(180deg,#cbf9ff,#b2f2fb)] ease-out [animation-delay:0.5s] md:left-1/3" />
         <div className="flex flex-col items-center justify-center gap-6">
           <div className="flex flex-row items-center justify-center gap-12">
             <div className="relative flex size-26 items-center justify-center">
