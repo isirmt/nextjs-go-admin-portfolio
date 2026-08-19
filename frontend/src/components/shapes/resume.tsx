943915d043cefd8165928d0e55d4bfea471bc46d
isirmt <52734487+isirmt@users.noreply.github.com>
2026-08-20 03:47:26 +0900

    add: icons

---
+++ frontend/src/components/shapes/resume.tsx
@@ -0,0 +1,11 @@
+export default function Resume({ className }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 256 256"
+      className={className}
+    >
+      <path d="M184.38,118.24l-86.65-50.03c-7.51-4.34-16.9,1.08-16.9,9.76v100.06c0,8.68,9.39,14.1,16.9,9.76l86.65-50.03c7.51-4.34,7.51-15.18,0-19.52Z" />
+    </svg>
+  );
+}
