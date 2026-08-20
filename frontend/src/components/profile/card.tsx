b7b0b313886d11b5f37ca50d95b9fdcf75d61c04
isirmt <52734487+isirmt@users.noreply.github.com>
2026-08-20 22:36:02 +0900

    fiux: auto wrapping in card

--- frontend/src/components/profile/card.tsx
+++ frontend/src/components/profile/card.tsx
@@ -160,7 +160,7 @@ export default function ProfileCard({ age }: ProfileCardProps) {
               <div
                 className={`${kiwiMaru.className} flex w-fit flex-col gap-3.5 text-base tracking-wide text-[#61230b]`}
               >
-                <div className="flex gap-2">
+                <div className="flex flex-wrap gap-2">
                   <div>兵庫県神戸市出身</div>
                   <div>
                     2004年 3月 8日生の&nbsp;
